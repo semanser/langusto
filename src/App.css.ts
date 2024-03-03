cec6af6bcc29851ee143f0aa26374ad27d4abe74
Andriy Semenets <semanser@gmail.com>
2024-03-03 18:31:48 +0100

    Fix styles

--- src/App.css.ts
+++ src/App.css.ts
@@ -2,7 +2,7 @@ import { style } from "@vanilla-extract/css";
 
 export const wrapperStyles = style({
   maxWidth: "450px",
-  width: "100%",
+  width: "90%",
   margin: "0 auto",
   marginTop: "220px",
 });
